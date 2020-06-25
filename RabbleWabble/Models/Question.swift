cf416f501fbce64d2d9bdec31c5cbcba7f94dbcd
Mohamed Samir Salem <53972048+mohamedsamirmohamedsalem@users.noreply.github.com>
2020-06-25 11:06:29 +0200

    first commit

---
+++ RabbleWabble/Models/Question.swift
@@ -0,0 +1,9 @@
+//
+//  Question.swift
+//  RabbleWabble
+//
+//  Created by Mohamed Samir on 6/21/20.
+//  Copyright © 2020 Mohamed Samir. All rights reserved.
+//
+
+import Foundation
---
+++ RabbleWabble/Views/QuestionView.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionView.swift
+//  RabbleWabble
+//
+//  Created by Cast Group on 02/07/2018.
+//  Copyright © 2018 br.hoffsilva. All rights reserved.
+//
+
+import UIKit
+
+public class QuestionView: UIView {
+    @IBOutlet public var answerLabel: UILabel!
+    @IBOutlet public var correctCountLabel: UILabel!
+    @IBOutlet public var incorrectCountLabel: UILabel!
+    @IBOutlet public var promptLabel: UILabel!
+    @IBOutlet public var hintLabel: UILabel!
+}
